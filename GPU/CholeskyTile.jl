d9730d4e69ceb83ddbed1aede4f6904e7cedc16b
Hao Zhu <oasisks2@gmail.com>
2023-12-29 13:31:35 -0500

    Fixed an error regarding dtrsm

--- GPU/CholeskyTile.jl
+++ GPU/CholeskyTile.jl
@@ -17,7 +17,7 @@ using KernelAbstractions, CUDA
     if I_g == current_group && J_g == current_group && currentI >= currentJ && currentJ == current_col
         temp_sum = zero(eltype(matrix))
 
-        @inbounds begin
+        begin
             for _j = currentJ-J+1:currentJ
                 temp_sum += output[currentI, _j] * conj(output[currentI, _j])
             end
@@ -33,26 +33,30 @@ using KernelAbstractions, CUDA
     end
 end
 
-# @kernel function cholesky_kernel!(matrix, output, col_start, current_col, row_start, row_end)
-#     I, J = @index(Global, NTuple)
+@kernel function dtrsm_kernel!(matrix, output, tilesize, k, t, A)
+    I_g, J_g = @index(Group, NTuple)
+    I, J = @index(Local, NTuple)
 
-#     if J == current_col && row_start <= I <= row_end && I >= J
-#         temp_sum = zero(eltype(matrix))
+    i, j = (I_g * tilesize - (tilesize) + I, J_g * tilesize - (tilesize) + J)
 
-#         @inbounds begin
-#             for _j = 1:J
-#                 temp_sum += output[I, _j] * conj(output[I, _j])
-#             end
-#         end
+    # we only consider blocks in the kth column
+    # and only the column right below the (k, k) tile
+    if J_g == k && I_g > k
+        tilestart = J_g * tilesize - (tilesize - 1)
+        tileend = J_g * tilesize
 
-#         # this is on the diagonal
-#         if I == J
-#             output[I, J] = sqrt(matrix[I, J] - temp_sum)
-#         else
-#             output[I, J] = (1.0 / output[J, J] * (matrix[I, J] - temp_sum))
-#         end
-#     end
-# end
+        temp_sum = zero(eltype(matrix))
+        # note all are square matrices
+        A_j = convert(Int, j - tilesize * (k - 1))
+        for _j = tilestart:tileend
+            A_i = convert(Int, _j - tilesize * (k - 1))
+            temp_sum += matrix[i, _j] * A[A_i, A_j]
+        end
+
+        output[i, j] = temp_sum
+    end
+
+end
 
 """
 This function performs the usual cholesky decomposition algorithm 
@@ -70,10 +74,26 @@ function dpotrf!(matrix, output, tilesize, k)
         kernel!(matrix, output, k, _j, tilesize, ndrange=size(matrix), workgroupsize=(tilesize, tilesize))
         KernelAbstractions.synchronize(backend)
     end
-    # for _j = j:j_e
-    #     kernel!(matrix, output, j, _j, i, i_e, ndrange=size(matrix))
-    #     KernelAbstractions.synchronize(backend)
-    # end
+end
+
+
+"""
+This function performs ((A \\ B')') where A is the diagonal tile and the Bs are the column tiles
+"""
+function dtrsm!(matrix, output, tilesize, k, t)
+    # first the 'output' contains the A matrix
+    # second, the 'matrix' contains all the B's we need for the columns
+    backend = KernelAbstractions.get_backend(matrix)
+    kernel! = dtrsm_kernel!(backend)
+
+    # # note this is the makeshift approach
+    tile = convert(Int, tilesize * k - (tilesize - 1))
+    tile_e = convert(Int, tilesize * k)
+    original = view(output, tile:tile_e, tile:tile_e)
+    A = inv(original)'
+    display(A)
+    kernel!(matrix, output, tilesize, k, t, A, ndrange=size(matrix), workgroupsize=(tilesize, tilesize))
+    KernelAbstractions.synchronize(backend)
 end
 
 function CholesktFactorization(matrix, output, tilesize=2)
@@ -92,12 +112,16 @@ function CholesktFactorization(matrix, output, tilesize=2)
     end
 
     for k = 1:t
-        # println("Current Block ", k)
+        # perform the cholesky on the diagonal tile
         dpotrf!(matrix, output, tilesize, k)
+
+        # now we want to parallize the entire kth block column
+        dtrsm!(matrix, output, tilesize, k, t)
+        break
     end
 end
 
-n = 4
+n = 6
 A = RandomHermitianMatrixInt64(n)
 juliaImplementation = cholesky(A)
 
